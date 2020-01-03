97456be4fe92b5bdf04677778e463a93075066c0
captproton <carl@wdwhub.net>
2020-01-02 20:14:59 -0800

    CHANGE: add_accepted_terms_to_message.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_03_030706) do
+ActiveRecord::Schema.define(version: 2020_01_03_040255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_01_03_030706) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "accepted_terms"
   end
 
   create_table "notifications", force: :cascade do |t|
